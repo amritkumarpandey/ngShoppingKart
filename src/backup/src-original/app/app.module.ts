d8f67a90763e1766464c8321cd7c643507506b72
amritkumarpandey <46307127+amritkumarpandey@users.noreply.github.com>
2019-01-01 22:53:23 -0500

    services

--- src/backup/src-original/app/app.module.ts
+++ src/backup/src-original/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
