5f186357277abd51e9fdb4814e5ea01e4c564b09
amritkumarpandey <46307127+amritkumarpandey@users.noreply.github.com>
2019-02-03 01:46:38 -0500

    router changes

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.server = this.serversService.getServer(1);
+  }
+
+  onReload() : void {
+    //this.router.navigate(['servers'],{relativeTo:this.route});
+    this.router.navigate(['servers']);
+  }
+}
